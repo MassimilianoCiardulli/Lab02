5d95835517a89a84826d96b4dbdda4ad83bc49e0
Massimiliano <Massimiliano@Massimiliano.lan>
2018-03-15 23:37:35 +0100

    Prima versione

--- Lab2_Alien/src/it/polito/tdp/alien/AlienController.java
+++ Lab2_Alien/src/it/polito/tdp/alien/AlienController.java
@@ -4,10 +4,11 @@ package it.polito.tdp.alien;
  * Sample Skeleton for 'Alien.fxml' Controller Class
  */
 
-
-
 import java.net.URL;
+import java.util.List;
+import java.util.NoSuchElementException;
 import java.util.ResourceBundle;
+import java.util.StringTokenizer;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
@@ -16,6 +17,8 @@ import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 
 public class AlienController {
+	
+	private AlienDictionary dictionary;
 	
     @FXML
     private ResourceBundle resources;
@@ -38,18 +41,61 @@ public class AlienController {
     	assert btnTranslate != null : "fx:id=\"bntTranslate\" was not injected: check your FXML file 'Alien.fxml'.";
     	assert btnReset != null : "fx:id=\"btnReset\" was not injected: check your FXML file 'Alien.fxml'.";
     	
+    	dictionary = new AlienDictionary();
     }
   
     
     @FXML
     void doTranslate(ActionEvent event) {
-    	    	
+    	txtResult.clear();    
+    	String inserito = txtWord.getText();
+    	StringTokenizer st = new StringTokenizer(inserito,">");
+    	
+    	String a = st.nextToken().trim();
+    	
+    	if(this.controlla(a)) {
+    		String alieno = a.substring(1, a.length()).toLowerCase();
+        	System.out.println(alieno);
+        	String b = "";
+        	try {
+        		b = st.nextToken().trim();
+        	} catch(NoSuchElementException e) {
+        		List<String> t = (List<String>) dictionary.translateWord(alieno);
+        		if(t == null) {
+        			txtResult.setText("Non ho trovato la parola...");
+        			txtWord.setText("");
+        			return;
+        		}
+        		StringBuilder sb = new StringBuilder();
+        		sb.append("");
+        		for(String s:t)
+        			sb.append(s + "\n");
+        		txtResult.setText(sb.toString());
+        		txtWord.setText("");
+        		return;
+        	}
+        	if(this.controlla(b)) {
+        		String trad = b.substring(1, b.length()).toLowerCase();
+        		System.out.println(trad);
+        		dictionary.addWord(alieno, trad);
+        		txtWord.setText("");
+        	}
+        	
+        	
+    	}
+    	
+    	
     }
     
     
     @FXML
     void doReset(ActionEvent event) {
-
+    	txtResult.clear();
     }
     
+    public boolean controlla(String toCheck) {
+    	if(toCheck.charAt(0) == '<')
+    		return true;
+    	return false;
+    }
 }
